8fa9d9237db0fc70619afc2f45deabb5689f58f5
UA2004 <ua2004@ukr.net>
2014-10-27 10:16:28 +0200

    Added main scene

---
+++ Assets/OnClickScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnClickScript : MonoBehaviour {
+
+	// Use this for initialization
+	public void FinalAnswer () {
+		Debug.Log("Final answer accepted!");
+
+	}
+}

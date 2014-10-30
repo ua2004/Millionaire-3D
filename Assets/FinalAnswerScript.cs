e2eca046de50876ff02f81e987e142a66c5fbee8
UA2004 <ua2004@ukr.net>
2014-10-30 07:47:32 +0200

    Added FinalAnswerScript.cs

---
+++ Assets/FinalAnswerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalAnswerScript : MonoBehaviour {
+
+	//public Color32 answerCaptionColorUnselected = new Color(247, 154, 28, 255);
+	//public Color32 answerCaptionColorSelected = new Color(255, 255, 255, 255);
+
+	public void OnFinalAnswerClick(int answer)
+	{
+		Debug.Log(answer);
+	}
+
+	/*public void ObjectTest(Object ob)
+	{
+
+	}*/
+}

4be8be14eaaa0e4eb5746c01aa1513c6acb73f9a
UA2004 <ua2004@ukr.net>
2014-11-03 00:42:47 +0200

    Added GameProcessScript which is the main script for controlling the game state

--- Assets/Scripts/GameProcessScript.cs
+++ Assets/Scripts/GameProcessScript.cs
@@ -1,11 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum State
+{
+	RULE_EXPLANATION,
+	READING_QUESTION,
+	FULL_QUESTION,
+	FINAL_ANSWER_GIVEN,
+	CORRECT_ANSWER,
+	WRONG_ANSWER,
+	USING_LIFELINE,
+	MONEY_TAKEN,
+};
+
 public class GameProcessScript : MonoBehaviour {
 
-	// Use this for initialization
+	public State state; // current game state
+	//some answers may be unavailable after using 50x50 lifeline
+	public bool[] isAnswerAvailable = new bool[4];
+
+	// When the game starts
 	void Start () {
-	
+		this.state = State.FULL_QUESTION;
+		for (int i=0; i<4; i++)
+		{
+			this.isAnswerAvailable[i] = true;
+		}
 	}
 	
 	// Update is called once per frame

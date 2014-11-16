7fc7393643769ca098d74c3e6e233f35b00b1b26
UA2004 <ua2004@ukr.net>
2014-11-16 19:13:17 +0200

    Added constructor in 'Question' class

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class Question {
 
-	public string text; //question text
+	public string question; //question text
 	public string[] answers = new string[4]; //array of 4 answers
+	public int correctAnswer; //0 means A, 1 means B, etc.
+	public string correctAnswerText; //text of the correct answer
 	public int number; //question number (usually 1-15)
-	public int price; //money that will bring the correct answer to this question
-	public int level; //difficulty level (usually 1-3 where 1 means quesions 1-5, 2 means 6-10 etc.)
+	public string synopsis; //short explanation of the correct answer which is shown after user gave his final answer
+
+	public Question(string question, string[] answers, int correctAnswer, string synopsis)
+	{
+		this.question = question;
+		this.answers = answers;
+		this.correctAnswer = correctAnswer;
+		this.correctAnswerText = this.answers[this.correctAnswer];
+		this.synopsis = synopsis;
+	}
 }

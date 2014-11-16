3ce1f59fb711134ae7bdcdb295d066b159c8878f
UA2004 <ua2004@ukr.net>
2014-11-16 19:11:59 +0200

    Created 'GameFormat' base class and 'ClassicGameFormat' derived class

---
+++ Assets/Scripts/ClassicGameFormat.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassicGameFormat : GameFormat {
+
+	public ClassicGameFormat()
+	{
+		this.moneyTree = new int[]{
+			100, //first question prize
+			200, //second question prize
+			300, //etc.
+			500,
+			1000,
+			2000,
+			4000,
+			8000,
+			16000,
+			32000,
+			64000,
+			125000,
+			250000,
+			500000,
+			1000000,
+		};
+
+		this.guaranteedPrizes = new int[]{
+			5,
+			10,
+		};
+	}
+}
---
+++ Assets/Scripts/GameFormat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Base game format class. If you create your own game format then its class
+ * should extend this one.
+ */
+public abstract class GameFormat {
+
+	//array of money prizes for each question
+	/*private readonly int[] moneyTree;
+	public int[] MoneyTree {
+		get { return this.moneyTree; }
+	}*/
+	public int[] moneyTree;
+
+	//array which contains question numbers which bring the player guaranteed prizes
+	/*private readonly int[] guaranteedPrizes;
+	public int[] GuaranteedPrizes {
+		get { return this.guaranteedPrizes; }
+	}*/
+	public int[] guaranteedPrizes;
+}

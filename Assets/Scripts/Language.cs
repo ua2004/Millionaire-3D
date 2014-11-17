c1faa66e9a1281e43aa7761ab86f63eb27e10574
UA2004 <ua2004@ukr.net>
2014-11-17 08:16:37 +0200

    Added private fields 'strings' and 'defaultStrings' and 't' method to 'Language' class

--- Assets/Scripts/Language.cs
+++ Assets/Scripts/Language.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -10,6 +11,8 @@ public class Language {
 	 */
 	public string code;
 	public const string DefaultLanguage = "en-US";
+	private Dictionary<string, string> strings;
+	private Dictionary<string, string> defaultStrings;
 
 	public Language()
 	{
@@ -29,22 +32,52 @@ public class Language {
 	private void loadLanguageFile()
 	{
 		TextAsset textAsset = (TextAsset) Resources.Load("Strings/" + this.code);
-		if(!textAsset) //if language file doesn't exist
+		this.strings = new Dictionary<string, string>();
+		if(textAsset) // if language file exists
 		{
-			//Debug.Log("Asset not found. Loading en-US...");
-			textAsset = (TextAsset) Resources.Load("Strings/en-US");
+			XmlDocument xmldoc = new XmlDocument();
+			xmldoc.LoadXml(textAsset.text.Replace("\n", string.Empty ));
+			foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
+			{
+				this.strings.Add(node.Attributes.GetNamedItem("id").Value, node.InnerXml);
+			}
+		}
+
+		textAsset = (TextAsset) Resources.Load("Strings/" + Language.DefaultLanguage);
+		this.defaultStrings = new Dictionary<string, string>();
+		if(textAsset) // if default language file exists
+		{
+			XmlDocument xmldoc = new XmlDocument();
+			xmldoc.LoadXml(textAsset.text.Replace("\n", string.Empty ));
+			foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
+			{
+				this.defaultStrings.Add(node.Attributes.GetNamedItem("id").Value, node.InnerXml);
+			}
+		}
+		else // if default language file doesn't exist
+		{
+			/**
+			 * @todo Exception must call message box
+			 */
+			throw new UnityException("Def lang file doesn't exist!");
+		}
+	}
+
+	/**
+	 * Returns language string with given ID.
+	 * 
+	 * @param string id String ID in XML language file
+	 * @return string
+	 */
+	public string t(string id)
+	{
+		if (this.strings.ContainsKey(id))
+		{
+			return this.strings[id];
 		}
 		else
 		{
-			//Debug.Log ("Asset found!");
+			return id;
 		}
-		//Debug.Log(textAsset);
-		
-		/*XmlDocument xmldoc = new XmlDocument();
-		xmldoc.LoadXml(textAsset.text.Replace("\n", String.Empty));
-		foreach (XmlNode node in xmldoc.DocumentElement.ChildNodes)
-		{
-			//Debug.Log(node);
-		}*/
 	}
 }

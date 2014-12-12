63fb24ce4da69986a7197a6f34b3eb310dcb51c1
UA2004 <ua2004@ukr.net>
2014-12-12 23:29:21 +0200

    Added 'finalAnswer' and removed 'number' field in 'Question' class

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -5,9 +5,9 @@ public class Question {
 
 	public string question; //question text
 	public string[] answers = new string[4]; //array of 4 answers
+	public int finalAnswer; //0 means A, 1 means B, etc.
 	public int correctAnswer; //0 means A, 1 means B, etc.
 	public string correctAnswerText; //text of the correct answer
-	public int number; //question number (usually 1-15)
 	public string synopsis; //short explanation of the correct answer which is shown after user gave his final answer
 
 	public Question(string question, string[] answers, int correctAnswer, string synopsis)

10b96a3b50cb9f5579c7199418c0b4ab11cc3a60
UA2004 <ua2004@ukr.net>
2014-12-13 21:44:29 +0200

    Created 'Extensions' class with extension methods for some standard Unity classes

---
+++ Assets/Classes/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Class which contains extension methods for some Unity classes.
+ * Must be static.
+ */
+public static class Extensions {
+	
+	/**
+	 * Sets only X coordinate of object's position remaining Y and Z untouched.
+	 * First parameter must have "this" in front of type.
+	 */
+	public static void SetPositionX(this Transform t, float newX)
+	{
+		t.position = new Vector3(newX, t.position.y, t.position.z);
+	}
+
+	/**
+	 * Destroys all children of specified object.
+	 * @return Transform
+	 */
+	public static Transform DestroyChildren(this Transform transform)
+	{
+		foreach (Transform child in transform) {
+			GameObject.Destroy(child.gameObject);
+		}
+		return transform;
+	}
+}

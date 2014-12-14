f5016a7be93b31c2dd07ef16aa559dc3bf8f4c1c
UA2004 <ua2004@ukr.net>
2014-12-14 23:56:23 +0200

    'LoadQuestion' method moved from 'GameProcessScript' class to 'Question' class

--- Assets/Scripts/GameProcessScript.cs
+++ Assets/Scripts/GameProcessScript.cs
@@ -18,7 +18,6 @@ public enum State
 public class GameProcessScript : MonoBehaviour {
 
 	public Language l; //current game language chosen by user
-	private SqliteDatabase db; //SQLite question database
 	public State state; // current game state
 	//some answers may be unavailable after using 50x50 lifeline
 	public bool[] isAnswerAvailable = new bool[4];
@@ -26,43 +25,44 @@ public class GameProcessScript : MonoBehaviour {
 	public int difficlutyLevel;
 	public int questionNumber;
 	public GameFormat gameFormat;
-	public Animator[] answerAnimation; // animators for each answer, specified in Unity editor
 
-	public Image currentPrizeImage; // image with current prize text, it is shown after
+	public Image logenze; // logenze image, it contains question and 4 answers
+	public Image currentPrizeImage; // image with current prize text, it is shown after correct answer
 
 	// When the game starts
-	void Start () {
+	void Start ()
+	{
 		this.l = new Language("uk-UA");
 		this.StartGame();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
 	public void StartGame()
 	{
-		this.db = new SqliteDatabase("questions.bytes");
 		this.gameFormat = new ClassicGameFormat();
 		this.difficlutyLevel = 1;
 		this.questionNumber = 1;
-		LoadQuestion();
+		this.question = new Question();
+		this.state = State.WAITING_ANSWER;
 	}
 
 	public void AnswerSelected(int answerNumber)
 	{
 		if( (this.state == State.WAITING_ANSWER) && (this.isAnswerAvailable[answerNumber]) )
 		{
-			//answerAnimation.Play("FinalAnswer");
 			this.state = State.FINAL_ANSWER_GIVEN;
 			this.question.finalAnswer = answerNumber;
-			this.answerAnimation[answerNumber].Play("FinalAnswer");
-			StartCoroutine("RevealAnswer");
+			//this.answerAnimation[answerNumber].Play("FinalAnswer");
+			//StartCoroutine("RevealAnswer");
 		}
 	}
 
-	public IEnumerator RevealAnswer()
+	/*public IEnumerator RevealAnswer()
 	{
 		// wait 3 to 6 seconds before revealing correct answer
 		yield return new WaitForSeconds(Random.Range(3f, 6f));
@@ -77,12 +77,20 @@ public class GameProcessScript : MonoBehaviour {
 			else
 			{
 				this.state = State.CORRECT_ANSWER;
-				//GameObject btn = (GameObject) Object.Instantiate(currentPrizeImage);
 				Debug.Log("Correct! You won " + this.gameFormat.GetPrizeForQuestion(this.questionNumber));
 				this.answerAnimation[this.question.finalAnswer].Play("CorrectAnswer");
+				//showing current prize bar
+				Image prize = (Image) Object.Instantiate(this.currentPrizeImage);
+				prize.transform.SetParent(GameObject.Find("Canvas").transform, false);
+				Text prizeText = prize.GetComponentInChildren<Text>();
+				prizeText.text = this.l.FormatPrize(this.gameFormat.GetPrizeForQuestion(this.questionNumber));
+
 				yield return new WaitForSeconds(3);
+				prize.transform.DestroyChildren();
+				Destroy(prize);
+
 				this.questionNumber++;
-				this.LoadQuestion();
+				this.question = new Question();
 			}
 		}
 		else
@@ -91,53 +99,5 @@ public class GameProcessScript : MonoBehaviour {
 			this.answerAnimation[this.question.correctAnswer].Play("WrongAnswer");
 			Debug.Log("Wrong! Your total prize is " + this.gameFormat.GetGuaranteedPrizeForQuestion(this.questionNumber));
 		}
-	}
-
-	/**
-	 * Selects one random question from database.
-	 */ 
-	public void LoadQuestion()
-	{
-		string query = "SELECT * FROM `questions` WHERE `difficulty_level`='" + this.difficlutyLevel + "' ORDER BY RANDOM() LIMIT 1;";
-		DataTable result = this.db.ExecuteQuery(query);
-		DataRow row = result[0];
-		this.question = new Question(
-			row["question"].ToString(),
-			new string[] {row["answer1"].ToString(), row["answer2"].ToString(), row["answer3"].ToString(), row["answer4"].ToString()},
-			int.Parse(row["correct_answer"].ToString()),
-			row["synopsis"].ToString()
-		);
-
-		Text questionText = (Text) GameObject.Find("QuestionText").GetComponent<Text>();
-		questionText.text = this.question.question;
-		Text ansAText = (Text) GameObject.Find("AnsAText").GetComponent<Text>();
-		ansAText.text = this.question.answers[0];
-		Text ansBText = (Text) GameObject.Find("AnsBText").GetComponent<Text>();
-		ansBText.text = this.question.answers[1];
-		Text ansCText = (Text) GameObject.Find("AnsCText").GetComponent<Text>();
-		ansCText.text = this.question.answers[2];
-		Text ansDText = (Text) GameObject.Find("AnsDText").GetComponent<Text>();
-		ansDText.text = this.question.answers[3];
-
-		for(int i=0; i<=3; i++)
-		{
-			this.isAnswerAvailable[i] = true;
-			this.answerAnimation[i].Play("ActiveAnswer");
-		}
-		this.state = State.WAITING_ANSWER;
-
-		//Debug.Log(this.question.synopsis);
-		//Debug.Log("Question: " + q.question + " Correct answer: " + q.correctAnswer + " - " + q.correctAnswerText);
-	}
-
-	/**
-	 * Returns number of available questions in database.
-	 * 
-	 * @todo It should count only questions for current language and category.
-	 */
-	private int QuestionCount()
-	{
-		string query = "SELECT COUNT(*) AS `count` FROM `questions`";
-		return (int)this.db.ExecuteQuery(query)[0]["count"];
-	}
+	}	*/
 }
--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Question {
@@ -9,13 +10,77 @@ public class Question {
 	public int correctAnswer; //0 means A, 1 means B, etc.
 	public string correctAnswerText; //text of the correct answer
 	public string synopsis; //short explanation of the correct answer which is shown after user gave his final answer
+	public Animator[] answerAnimation; // animators for each answer
 
-	public Question(string question, string[] answers, int correctAnswer, string synopsis)
+	private SqliteDatabase db; //SQLite question database
+
+	/*public Question(string question, string[] answers, int correctAnswer, string synopsis)
 	{
 		this.question = question;
 		this.answers = answers;
 		this.correctAnswer = correctAnswer;
 		this.correctAnswerText = this.answers[this.correctAnswer];
 		this.synopsis = synopsis;
+	}*/
+
+	public Question()
+	{
+		this.LoadQuestion();
+	}
+
+	/**
+	 * Selects one random question from database.
+	 */ 
+	protected void LoadQuestion()
+	{
+		this.db = new SqliteDatabase("questions.bytes");
+		string query = "SELECT * FROM `questions` WHERE `difficulty_level`='1' ORDER BY RANDOM() LIMIT 1;";
+		DataTable result = this.db.ExecuteQuery(query);
+		DataRow row = result[0];
+		this.question = row["question"].ToString();
+		this.answers = new string[] {row["answer1"].ToString(), row["answer2"].ToString(), row["answer3"].ToString(), row["answer4"].ToString()};
+		this.correctAnswer = int.Parse(row["correct_answer"].ToString());
+		this.correctAnswerText = this.answers[this.correctAnswer];
+		this.synopsis = row["synopsis"].ToString();
+
+		//instantiating logenze from prefab
+		GameObject logenze = (GameObject) Object.Instantiate((GameObject) Resources.Load("Prefabs/Classic/Logenze", typeof(GameObject)));
+		logenze.transform.SetParent(GameObject.Find("Canvas").transform, false);
+		//removing "(Clone)" suffix from name
+		logenze.transform.name = logenze.transform.name.Replace("(Clone)","").Trim();
+
+		Text questionText = (Text) GameObject.Find("QuestionText").GetComponent<Text>();
+		questionText.text = this.question;
+		Text ansAText = (Text) GameObject.Find("AnsAText").GetComponent<Text>();
+		ansAText.text = this.answers[0];
+		Text ansBText = (Text) GameObject.Find("AnsBText").GetComponent<Text>();
+		ansBText.text = this.answers[1];
+		Text ansCText = (Text) GameObject.Find("AnsCText").GetComponent<Text>();
+		ansCText.text = this.answers[2];
+		Text ansDText = (Text) GameObject.Find("AnsDText").GetComponent<Text>();
+		ansDText.text = this.answers[3];
+		
+		this.answerAnimation[0] = (Animator) GameObject.Find("AnsAImage").GetComponent<Animator>();
+		this.answerAnimation[1] = (Animator) GameObject.Find("AnsBImage").GetComponent<Animator>();
+		this.answerAnimation[2] = (Animator) GameObject.Find("AnsCImage").GetComponent<Animator>();
+		this.answerAnimation[3] = (Animator) GameObject.Find("AnsDImage").GetComponent<Animator>();
+		for(int i=0; i<=3; i++)
+		{
+			this.answerAnimation[i].Play("ActiveAnswer");
+		}
+		
+		//Debug.Log(this.question.synopsis);
+		//Debug.Log("Question: " + q.question + " Correct answer: " + q.correctAnswer + " - " + q.correctAnswerText);
+	}	
+
+	/**
+	 * Returns number of available questions in database.
+	 * 
+	 * @todo It should count only questions for current language and category.
+	 */
+	protected int Count()
+	{
+		string query = "SELECT COUNT(*) AS `count` FROM `questions`";
+		return (int)this.db.ExecuteQuery(query)[0]["count"];
 	}
 }

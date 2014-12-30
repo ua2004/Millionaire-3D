d9c750e3cb58865c84869ed2c73476e59215ebba
UA2004 <ua2004@ukr.net>
2014-12-30 08:38:55 +0200

    Added 'Lifeline' base class for all future lifelines

---
+++ Assets/Scripts/Lifeline.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Base class for all lifelines.
+ */
+public abstract class Lifeline {
+
+	/**
+	 * Must be overridden in derived classes.
+	 */
+	public abstract void Use();
+}
